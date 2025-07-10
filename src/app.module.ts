d72bfe63a6a502a5442296873625d646d419fcb8
PMota <pvmota012@gmail.com>
2025-07-09 22:46:01 -0300

    feat: create game module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GameModule } from './game/game.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true, // Makes the configuration available globally
+        }),
+        GameModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
-
 export class AppModule {}
