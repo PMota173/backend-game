ff6231fe5cac5a7fc9efe59c881fdc87e8b1ffa1
PMota <pvmota012@gmail.com>
2025-07-14 22:16:04 -0300

    feat: configure websocket gateway

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -1,9 +1,42 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+    OnGatewayInit,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    SubscribeMessage,
+    WebSocketGateway,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class GameGateway {
+@WebSocketGateway({
+    cors: {
+        origin: '*', // Allow all origins for CORS
+        methods: ['GET', 'POST'], // Allow GET and POST methods
+        credentials: true, // Allow credentials
+    },
+})
+export class GameGateway
+    implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+    afterInit(server: Server) {
+        console.log('WebSocket server initialized', server);
+    }
+
+    handleConnection(client: Socket) {
+        console.log('Client connected:', client.id);
+    }
+
+    handleDisconnect(client: Socket) {
+        console.log('Client disconnected:', client.id);
+    }
+
+    constructor() {
+        console.log('GameGateway initialized');
+    }
+
     @SubscribeMessage('message')
     handleMessage(client: any, payload: any): string {
+        console.log('Received message:', payload);
+
         return 'Hello world!';
     }
 }
