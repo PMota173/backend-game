c8d0db470df2d3495f496f5351d455b3e9731b6f
PMota <pvmota012@gmail.com>
2025-07-16 14:13:43 -0300

    fix: better implementation for roomId generation

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -14,21 +14,16 @@ export class GameService {
     private gameRooms: Map<string, GameRoom> = new Map();
 
     createGame(payload: CreatingGamePayload): GameRoom {
-        const roomId = Math.floor(
-            10000000 + Math.random() * 90000000,
-        ).toString();
-
-        if (this.gameRooms.has(roomId)) {
-            // This is a very rare case, but it's good to handle it
-            this.logger.warn(`Generated duplicate roomId: ${roomId}. Retrying...`);
-            return this.createGame(payload); // Retry
-        }
+        let roomId: string;
+        do {
+            roomId = Math.floor(10000000 + Math.random() * 90000000).toString();
+        } while (this.gameRooms.has(roomId));
 
         const newGameRoom: GameRoom = {
             roomId,
             roomName: payload.roomName,
             players: [],
-            limit: 6,
+            limit: 8,
         };
 
         this.gameRooms.set(roomId, newGameRoom);
@@ -45,7 +40,9 @@ export class GameService {
 
         const gameRoom = this.gameRooms.get(roomId);
         if (!gameRoom) {
-            throw new NotFoundException(`Game room with ID ${roomId} not found.`);
+            throw new NotFoundException(
+                `Game room with ID ${roomId} not found.`,
+            );
         }
 
         if (gameRoom.players.length >= gameRoom.limit) {
